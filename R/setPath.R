bc79c003002ae694fe32a5419c62deb4ad44840e
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2019-05-07 18:29:08 +0200

    create new paths, if they don't exist yet

--- R/setPath.R
+++ R/setPath.R
@@ -4,8 +4,8 @@
 #' @param root [\code{character(1)}]\cr path tothe root directory that contains
 #'   all tables and sub-directories.
 #' @examples
-#' setPath(root = "/home/se87kuhe/Nextcloud/LUCKINet/data/")
-#' @importFrom checkmate assertDirectory
+#' setPath(root = "/home/se87kuhe/Nextcloud/LUCKINet/myProject/")
+#' @importFrom checkmate testDirectory
 #' @export
 
 setPath <- function(root = NULL){
@@ -16,7 +16,50 @@ setPath <- function(root = NULL){
   if(lastChar == "/"){
     root <- substr(root, start = 1, stop = nchar(root)-1)
   }
-  assertDirectory(root, access = "rwx")
-  options(dmt_path = root)
 
+  # test whether the required directories exist and create them if they don't exist
+  if(!testDirectory(x = root, access = "rw")){
+    dir.create(file.path(root))
+    message("I have created a new project directory.\n  -> please run 'setIndex()' to create the default and optional indices and translation tables.")
+  }
+
+  if(!testDirectory(x = file.path(root, "cT_census"), access = "rw")){
+    dir.create(file.path(root, "cT_census"))
+  }
+  if(!testDirectory(x = file.path(root, "cT_geometries"), access = "rw")){
+    dir.create(file.path(root, "cT_geometries"))
+  }
+
+  if(!testDirectory(x = file.path(root, "cT_census", "incoming"), access = "rw")){
+    dir.create(file.path(root, "cT_census", "incoming"))
+  }
+  if(!testDirectory(x = file.path(root, "cT_census", "meta"), access = "rw")){
+    dir.create(file.path(root, "cT_census", "meta"))
+  }
+  if(!testDirectory(x = file.path(root, "cT_census", "original_datasets"), access = "rw")){
+    dir.create(file.path(root, "cT_census", "original_datasets"))
+  }
+  if(!testDirectory(x = file.path(root, "cT_census", "stage1"), access = "rw")){
+    dir.create(file.path(root, "cT_census", "stage1"))
+  }
+  if(!testDirectory(x = file.path(root, "cT_census", "stage2"), access = "rw")){
+    dir.create(file.path(root, "cT_census", "stage2"))
+  }
+  if(!testDirectory(x = file.path(root, "cT_geometries", "incoming"), access = "rw")){
+    dir.create(file.path(root, "cT_geometries", "incoming"))
+  }
+  if(!testDirectory(x = file.path(root, "cT_geometries", "meta"), access = "rw")){
+    dir.create(file.path(root, "cT_geometries", "meta"))
+  }
+  if(!testDirectory(x = file.path(root, "cT_geometries", "original_datasets"), access = "rw")){
+    dir.create(file.path(root, "cT_geometries", "original_datasets"))
+  }
+  if(!testDirectory(x = file.path(root, "cT_geometries", "stage1"), access = "rw")){
+    dir.create(file.path(root, "cT_geometries", "stage1"))
+  }
+  if(!testDirectory(x = file.path(root, "cT_geometries", "stage2"), access = "rw")){
+    dir.create(file.path(root, "cT_geometries", "stage2"))
+  }
+
+  options(dmt_path = root)
 }
