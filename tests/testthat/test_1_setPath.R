f310cf4d6c5ec7915cdb12d325a58363c8c781d1
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2019-09-17 18:04:30 +0200

    wip testing

--- tests/testthat/test_1_setPath.R
+++ tests/testthat/test_1_setPath.R
@@ -3,15 +3,16 @@ library(checkmate)
 context("setPath")
 
 test_that("path has been added to the global options", {
-  # path <- system.file("test_datasets", package="arealDB", mustWork = TRUE)
-  # setPath(root = paste0(path, "/"))
-  #
-  # out <- getOption("adb_path")
-  # expect_character(x = out)
-  # expect_true(out == path)
+  path <- system.file("test_datasets", package="arealDB", mustWork = TRUE)
+  setPath(root = paste0(path, "/newProject"))
+
+  out <- getOption("adb_path")
+  expect_character(x = out)
+  expect_true(out == paste0(path, "/newProject"))
+
+  unlink(x = paste0(path, "/newProject"), recursive = TRUE)
 })
 
 test_that("Error if arguments have wrong value", {
-  expect_error(registerTables(root = 1))
+  expect_error(setPath(root = 1))
 })
-
