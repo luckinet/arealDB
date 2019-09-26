d875eb74c964f4676579982e60f36530046254fc
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2019-09-26 12:17:30 +0200

    rename tests

--- tests/testthat/test_5_regGeometry.R
+++ tests/testthat/test_5_regGeometry.R
@@ -1,15 +1,20 @@
 library(testthat)
 library(checkmate)
-context("newDataseries")
+context("regGeometry")
 
 
 test_that("", {
   path <- system.file("test_datasets", package="arealDB", mustWork = TRUE)
   setPath(root = paste0(path, "/newDB"))
 
+
   unlink(paste0(path, "/newDB"), recursive = TRUE)
 })
 
 test_that("Error if arguments have wrong value", {
+  path <- system.file("test_datasets", package="arealDB", mustWork = TRUE)
+  setPath(root = paste0(path, "/newDB"))
 
+
+  unlink(paste0(path, "/newDB"), recursive = TRUE)
 })
--- tests/testthat/test_6_regTable.R
+++ tests/testthat/test_6_regTable.R
@@ -7,9 +7,14 @@ test_that("", {
   path <- system.file("test_datasets", package="arealDB", mustWork = TRUE)
   setPath(root = paste0(path, "/newDB"))
 
+
   unlink(paste0(path, "/newDB"), recursive = TRUE)
 })
 
 test_that("Error if arguments have wrong value", {
+  path <- system.file("test_datasets", package="arealDB", mustWork = TRUE)
+  setPath(root = paste0(path, "/newDB"))
 
+
+  unlink(paste0(path, "/newDB"), recursive = TRUE)
 })
