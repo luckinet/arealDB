e6be1b740cb70cec42a383ccd2eea833c7ea8f13
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2019-10-01 14:33:02 +0200

    remove normalise

--- tests/testthat/test_normalise.R
+++
@@ -1,9 +0,0 @@
-context("normalise")
-
-test_that("", {
-  path <- system.file("test_datasets", package="arealDB", mustWork = TRUE)
-  setPath(root = paste0(path, "/newDB"))
-
-  unlink(paste0(path, "/newDB"), recursive = TRUE)
-})
-
