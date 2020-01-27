f211292d966dde793e8aa25eb13835a1f2cb1a7e
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2020-01-27 09:57:31 +0100

    View-function that removes the geom

--- R/helpers.R
+++ R/helpers.R
@@ -41,3 +41,15 @@ testCompressed <- function(x){
 
   return(grepl("^.*(.gz|.bz2|.tar|.zip|.tgz|.gzip|.7z)[[:space:]]*$", x))
 }
+
+#' View large simple features
+#' @param x [\code{sf}]\cr the sf object to view.
+#' @importFrom rlang ensym
+#' @export
+
+View_sf <- function(x){
+  theTitle <- ensym(x)
+  x %>% as_tibble() %>%
+    select(-geom) %>%
+    View(title = as.character(theTitle))
+}
