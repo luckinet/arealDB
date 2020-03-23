16b7182415f5d9d4b70236c3b263021d3b948d53
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2020-03-23 16:45:11 +0100

    update package

--- R/data.R
+++ R/data.R
@@ -1,14 +1,9 @@
-#' A table with country level metadata
+#' Simple feature geometries of countries
 #'
-#' @format The object of class tibble has 8 columns and 248 rows. It lists for
-#'   each country the \code{name}, \code{iso_a2} and \code{iso-a3}-code, whether
-#'   the country is an \code{un_member}, on which \code{continent} the country
-#'   is and of which \code{region} and \code{subregion} that is part. Finally,
-#'   it lists the initial ahID for each nation.
+#' @format The object of class sf has 10 columns and 248 rows. It lists for each
+#'   country the \code{unit} name, the \code{nation} name, \code{iso_a2} and
+#'   \code{iso-a3}-code, whether the country is an \code{un_member} or an
+#'   \code{island} on which \code{continent} the country is and of which
+#'   \code{region} and \code{subregion} that is part. Finally, it lists the
+#'   initial ahID for each nation.
 "countries"
-
-#' Simple feature geometries of all countries
-#'
-#' @format An object of class sf (inherits from tbl_df, tbl, data.frame) with
-#'   177 rows and 7 columns
-"countries_sf"
