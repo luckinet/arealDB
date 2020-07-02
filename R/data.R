9c7865820def97d4bcbc9f9e6039618223cd06f0
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2020-07-02 17:08:26 +0200

    fix description of internal dataset

--- R/data.R
+++ R/data.R
@@ -1,9 +1,9 @@
 #' Simple feature geometries of countries
 #'
-#' @format The object of class sf has 10 columns and 248 rows. It lists for each
+#' @format The object of class sf has 10 fields and 254 rows. It lists for each
 #'   country the \code{unit} name, the \code{nation} name, \code{iso_a2} and
-#'   \code{iso-a3}-code, whether the country is an \code{un_member} or an
-#'   \code{island} on which \code{continent} the country is and of which
+#'   \code{iso-a3}-code, whether the country is a \code{un_member} or an
+#'   \code{island}, on which \code{continent} the country is and of which
 #'   \code{region} and \code{subregion} that is part. Finally, it lists the
 #'   initial ahID for each nation.
 "countries"
