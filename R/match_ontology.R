024aa219ddcd9722da9610117a41861b7680928a
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2022-07-20 02:47:42 +0200

    vast revise

--- R/match_ontology.R
+++ R/match_ontology.R
@@ -9,253 +9,106 @@
 #'   territories are sourced.
 #' @param ontology [\code{onto}]\cr either a path where the ontology/gazetteer
 #'   is stored, or an already loaded ontology.
-#' @param from_meta [\code{logical(1)}]\cr whether or not to load the matches
-#'   from previous matching tables, or construct them from \code{table} and
-#'   \code{columns}.
 #' @param verbose [\code{logical(1)}]\cr whether or not to give detailed
 #'   information on the prcess of this function.
-#' @importFrom ontologics load_ontology get_concept new_concept new_mapping
-#'   new_source
-#' @importFrom purrr map
-#' @importFrom stringr str_replace_all
-#' @importFrom dplyr bind_cols
-#' @importFrom tidyr separate_rows separate
+#' @importFrom checkmate assertFileExists
+#' @importFrom ontologics load_ontology new_source get_concept new_mapping
+#' @importFrom purrr map_dfr
+#' @importFrom dplyr pull filter select mutate distinct bind_cols rename
+#'   everything left_join
+#' @importFrom tibble tibble
+#' @importFrom tidyselect all_of
+#' @importFrom tidyr separate_rows separate pivot_wider
 #' @importFrom sf st_drop_geometry
-#' @importFrom utils tail head
-#' @importFrom stats na.omit
 #' @export
 
 match_ontology <- function(table = NULL, columns = NULL, dataseries = NULL,
-                           ontology = NULL, from_meta = FALSE, verbose = FALSE){
+                           ontology = NULL, verbose = FALSE){
 
-  # table = temp; columns = unitCols; dataseries = dSeries; ontology = gazPath; from_meta = FALSE; verbose = FALSE
-
-  assertLogical(x = from_meta, len = 1, any.missing = FALSE)
-  assertLogical(x = verbose, len = 1, any.missing = FALSE)
-
+  # set internal paths
   intPaths <- paste0(getOption(x = "adb_path"))
 
+  # load the ontology
   if(inherits(x = ontology, what = "onto")){
-    gazPath <- NULL
+    gaz <- ontology
   } else {
     assertFileExists(x = ontology, access = "rw", extension = "rds")
-    gazPath <- ontology
-    theName <- tail(str_split(string = ontology, "/")[[1]], 1)
-    theName <- head(str_split(string = theName, pattern = "[.]")[[1]], 1)
-
-    gaz <- load_ontology(path = gazPath)
+    gaz <- load_ontology(path = ontology)
   }
 
-  theClasses <- gaz@classes
-  subClasses <- theClasses$harmonised$label[1:which(theClasses$harmonised$label == columns)]
-  parentClass <- theClasses$harmonised$label[which(theClasses$harmonised$label == columns) - 1]
+  # extract all non-harmoniesd concepts
+  temp <- map_dfr(.x = seq_along(columns), .f = function(ix){
 
-  # first, identify from where to take matches ----
-  if(from_meta){
+    table %>%
+      pull(columns[ix]) %>%
+      unique() %>%
+      tibble(label = ., class = columns[ix])
 
-    toMatch <- list.files(paste0(intPaths, "/meta/concepts"), full.names = TRUE)
+  }) %>%
+    filter(!is.na(label))
 
-  } else {
-    assertDataFrame(x = table)
-    assertCharacter(x = columns, any.missing = FALSE)
-    assertCharacter(x = dataseries, len = 1, any.missing = FALSE)
+  # make a new dataseries, in case it doesn't exist yet
+  if(!dataseries %in% gaz@sources$label){
+    gaz <- new_source(name = dataseries, ontology = gaz)
+  }
 
-    toMatch <- "table"
+  # extract all harmonised concepts, including those that may be not available
+  # (na) ...
+  harmonisedConc <- temp %>%
+    get_concept(x = ., na.rm = FALSE, ontology = gaz)
 
-    concepts <- map(.x = seq_along(columns), .f = function(ix){
+  # ... for if new concepts are still missing from the ontology, they need to be
+  # mapped
+  if(any(is.na(harmonisedConc$id))){
 
-      table %>%
-        pull(columns[ix]) %>%
-        unique()
+    gaz <- new_mapping(new = harmonisedConc$label, target = harmonisedConc %>% select(class), certainty = 3,
+                       source = dataseries, ontology = gaz, matchDir = paste0(intPaths, "/meta/concepts/"))
 
-    }) %>%
-      unlist()
-    concepts <- concepts[!is.na(concepts)]
+    write_rds(x = gaz, file = ontology)
 
-    # determine those concepts, that are not yet defined in the gazetteer
-    missingConcepts <- get_concept(x = tibble(label = concepts, class = columns), missing = TRUE, ontology = gaz, mappings = c("close", "narrower"))
-    inclConcepts <- get_concept(x = tibble(label = concepts, class = columns), ontology = gaz, mappings = c("close", "narrower"))
+  }
 
-    # build a table of external concepts and of harmonised concepts these should be overwritten with
-    if(dim(missingConcepts)[1] != 0){
+  # write the new concepts into 'table'
+  tab <- table %>%
+    st_drop_geometry()
 
-      relate <- gaz@concepts$harmonised %>%
-        select(id, label, class) %>%
-        left_join(inclConcepts, by = c("id", "label", "class")) %>%
-        # mutate(external_id = NULL) %>%
-        filter(!is.na(class)) %>%
-        filter(class %in% subClasses)
+  for(i in seq_along(columns)){
 
-      sortIn <- missingConcepts %>%
-        select(id, label, class, description, everything()) %>%
-        mutate(#external_id = NULL,
-               sort_in = label,
-               label = NA_character_,
-               class = NA_character_)
+    theColumn <- columns[i]
 
-      # put together the object that shall be edited by the user ...
-      sortIn %>%
-        bind_rows(relate) %>%
-        write_csv(file = paste0(intPaths, "/matching.csv"), quote = "all", na = "")
+    new <- tab %>%
+      select(label = theColumn) %>%
+      distinct() %>%
+      mutate(class = theColumn)
 
-      # ... and make them aware of their duty
-      message("please edit the file '", paste0(intPaths, "/matching.csv"), "'")
-      if(verbose){
-        message("column description and tips \n\ncode       : filter by this column to jump to the subset you need to edit\nharmonised : concepts to which the new terms should be related \nclass      : the class of harmonised concepts \nclose      : in case a new concept is a close match to the harmonised concept, paste \n             it next to that concept, delimit several concepts with a '|' \nnarrower     : in case a new concept is not the same as any harmonised concept, paste \n             it next to that concept into which it is nested, delimit \n             several concepts with a '|' \nsort_in    : cut out these concepts and sort them either into 'close' or into 'narrower' \n\n-> values that were already successfully matched by previous translations are listed here, \n   however, altering them here doesn't change the ontology. \n\n-> any row that doesn't contain a value in the column 'code' will be discarded. Hence, \n   if you want a value to be ignored, simply don't paste it anywhere. \n\n-> do not change the values in the columns 'code', 'harmonised' and 'class', as they \n   are important to insert the new matches into the ontology. \n\n-> if a term shall be nested into a position that doesn't have a class, (for example, \n   because that class occurrs the first time with this term) first create that nested \n   class with 'new_class()'.")
-      }
-      done <- readline(" -> press any key when done: ")
+    newConcepts <- get_concept(x = new, ontology = gaz, mappings = "all") %>%
+      rename(target = label) %>%
+      separate_rows(has_broader_match, has_close_match, has_exact_match, has_narrower_match, sep = " \\| ") %>%
+      bind_cols(new %>% select(!!theColumn := label)) %>%
+      mutate(target = if_else(!class %in% theColumn, !!sym(theColumn), target),
+             id = if_else(!class %in% theColumn, paste0(id, gaz@classes$harmonised$id[1]), id)) %>%
+      select(!!theColumn, target, id)
 
+    # ... and assign them to the respective column ...
+    if(i == 1){
+      temp <- table %>%
+        left_join(newConcepts, by = theColumn)
     } else {
-      # write an empty table
-      tibble(id = NA_character_,
-             note = NA_character_) %>%
-        write_csv(file = paste0(intPaths, "/matching.csv"), quote = "all", na = "")
-    }
-
-  }
-
-  # ... then align these matches with the ontology ----
-  for(i in seq_along(toMatch)){
-
-    if(from_meta){
-      related <- read_csv(toMatch[i], col_types = cols(.default = "c"))
-
-      dataseries <- tail(str_split(toMatch[i], "/")[[1]], 1)
-      dataseries <- str_split(dataseries, "_")[[1]]
-      dataseries <- str_split(dataseries[2], "[.]")[[1]][1]
-    } else {
-      related <- read_csv(paste0(intPaths, "/matching.csv"), col_types = cols(.default = "c")) %>%
-        filter(!is.na(id))
-
-      if(dim(related)[1] == 0){
-        related <- NULL
-      }
-    }
-
-    if(!is.null(related)){
-
-      # make a new dataseries, in case it doesn't exist yet
-      if(!dataseries %in% gaz@sources$label){
-        gaz <- new_source(name = dataseries, ontology = gaz)
-      }
-
-      nestedMatch <- related %>%
-        filter(!is.na(narrower)) %>%
-        separate_rows(narrower, sep = " \\| ") %>%
-        select(-close)
-
-      closeMatch <- related %>%
-        filter(!is.na(close)) %>%
-        separate_rows(close, sep = " \\| ")
-
-      # in case nested matches are present, derive the mappings table ...
-      if(dim(nestedMatch)[1] != 0){
-
-        newLvl <- get_concept(x = tibble(label = nestedMatch$harmonised, class = columns), ontology = gaz, tree = TRUE)
-        newLvl <- nestedMatch %>%
-          select(-class) %>%
-          left_join(newLvl %>% select(has_broader, class), by = c("id" = "has_broader")) %>%
-          group_by(narrower) %>%
-          summarise(newClass = unique(class)) %>%
-          ungroup()
-
-        nestedMatch <- nestedMatch %>%
-          left_join(newLvl, by = "narrower")
-
-        mappings <- get_concept(x = nestedMatch %>% select(label = harmonised, class), ontology = gaz) %>%
-          bind_cols(nestedMatch %>% select(new = narrower)) %>%
-          mutate(target = label,
-                 match = "narrower")
-      } else {
-        mappings <- tibble(id = character(), has_broader = character(), label = character(),
-                           class = character(), new = character(), target = character(), match = character())
-      }
-
-      # in case close matches are present, derive the mappings table ...
-      if(dim(closeMatch)[1] != 0){
-
-        mappings <- get_concept(x = closeMatch %>% select(label = harmonised, class), ontology = gaz) %>%
-          bind_cols(closeMatch %>% select(new = close)) %>%
-          mutate(target = label,
-                 match = "close") %>%
-          bind_rows(mappings)
-
-      }
-
-      # ... and store them in the ontology
-      gaz <- new_mapping(new = mappings$new,
-                         target = mappings %>% select(id, label = target, class),
-                         source = dataseries,
-                         match = mappings$match,
-                         certainty = 3,
-                         ontology = gaz)
-
-      write_rds(x = gaz, file = gazPath)
-    }
-
-  }
-
-  # ... and finally extract the respective concepts and merge them with the input table ----
-  if(!from_meta){
-
-    for(i in seq_along(columns)){
-
-      theColumn <- columns[i]
-
-      # finally, extract the harmonised concepts ...
-      newConcepts <- mappings %>%
-        select(new, target, id) %>%
-        rename(!!sym(theColumn) := new)
-
-      # ... and assign them to the respective column ...
-      if(i == 1){
-        temp <- table %>%
-          left_join(newConcepts, by = theColumn)
-      } else {
-        temp <- temp %>%
-          left_join(newConcepts, by = c(theColumn, "broader")) %>%
-          mutate(broader = code) %>%
-          select(-class, -sourceName, -code)
-      }
-
       temp <- temp %>%
-        select(-all_of(theColumn)) %>%
-        rename(!!theColumn := target)
-
-    }
-    out <- temp %>%
-      select(all_of(columns), id, everything())
-
-    # ... and store the newly defined matches as a dataseries specific matching table
-    if(testFileExists(paste0(intPaths, "/meta/concepts/match_", dataseries, ".csv"))){
-      prevMatches <- read_csv(paste0(intPaths, "/meta/concepts/match_", dataseries, ".csv"), col_types = cols(.default = "c"))
-    } else {
-      prevMatches <- NULL
+        mutate(broader = id) %>%
+        select(-id) %>%
+        left_join(newConcepts, by = theColumn)
     }
 
-    if(!is.null(related)){
+    temp <- temp %>%
+      select(-all_of(theColumn)) %>%
+      rename(!!theColumn := target)
 
-      newMatches <- mappings %>%
-        group_by(id, has_broader, label, class, match) %>%
-        summarise(new = paste0(new, collapse = " | ")) %>%
-        ungroup() %>%
-        pivot_wider(id_cols = c(id, has_broader, label, class), names_from = match, values_from = new) %>%
-        mutate(source = dataseries) %>%
-        select(id, harmonised = label, class, close, narrower, source) %>%
-        distinct(id, harmonised, class, close, narrower, source)
-
-      prevMatches %>%
-        bind_rows(newMatches) %>%
-        distinct() %>%
-        arrange(id) %>%
-        write_csv(file = paste0(intPaths, "/meta/concepts/match_", dataseries, ".csv"), append = FALSE, na = "")
-
-    }
-
-    return(out)
-
-  } else {
-    message("import successful.")
   }
+
+  out <- temp %>%
+    select(all_of(columns), id, everything())
+
+  return(out)
 
 }
