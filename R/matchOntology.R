30cfb242d347c4ba17ea6fc4d68f02b4853d56d9
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2023-01-24 12:28:25 +0100

    allow extraction exactly for classes, improve handling of nested concepts

--- R/matchOntology.R
+++ R/matchOntology.R
@@ -11,9 +11,15 @@
 #'   tentative columns (for debugging), or only the essential columns.
 #' @param ontology [\code{onto}]\cr either a path where the ontology/gazetteer
 #'   is stored, or an already loaded ontology.
+#' @param beep [\code{integerish(1)}]\cr Number specifying what sound to be
+#'   played to signal the user that a point of interaction is reached by the
+#'   program, see \code{\link[beepr]{beep}}.
+#' @param exact_class [`logical(1)`][logical]\cr whether or not to extract only
+#'   the classes in \code{table}, or also all parent classes.
 #' @param verbose [`logical(1)`][logical]\cr whether or not to give detailed
 #'   information on the process of this function.
 #' @importFrom checkmate assertFileExists
+#' @importFrom utils head
 #' @importFrom ontologics load_ontology new_source get_concept new_mapping
 #' @importFrom purrr map_dfr
 #' @importFrom dplyr pull filter select mutate distinct bind_cols rename
@@ -25,11 +31,18 @@
 #' @export
 
 matchOntology <- function(table = NULL, columns = NULL, dataseries = NULL,
-                          ontology = NULL, verbose = FALSE, all_cols = FALSE){
+                          ontology = NULL, verbose = FALSE, all_cols = FALSE,
+                          beep = NULL, exact_class = TRUE){
+
+  assertIntegerish(x = beep, len = 1, lower = 1, upper = 11, null.ok = TRUE)
 
   # set internal paths
   intPaths <- paste0(getOption(x = "adb_path"))
   ontoPath <- ontology
+
+  allCols <- get_class(ontology = ontoPath) %>%
+    pull(label)
+  allCols <- allCols[which(allCols %in% head(columns, 1)) : which(allCols %in% tail(columns, 1))]
 
   type <- str_split(tail(str_split(string = ontoPath, pattern = "/")[[1]], 1), "[.]")[[1]][1]
 
@@ -51,24 +64,16 @@ matchOntology <- function(table = NULL, columns = NULL, dataseries = NULL,
     tab <- table
   }
 
-  # replace values with an empty name with a placeholder
-  # for(i in seq_along(columns)){
-  #
-  #   theColumn <- columns[i]
-  #   tab <- tab %>%
-  #     mutate(across(all_of(theColumn), trimws),
-  #            unnamed = if_else(!!sym(theColumn) %in% c("", " "), TRUE, FALSE),
-  #            iter = cumsum(unnamed),
-  #            iter = if_else(unnamed, iter, 0L),
-  #            !!theColumn := if_else(!!sym(theColumn) %in% c("", " "), paste0("unnamed_", iter), !!sym(theColumn))) %>%
-  #     select(-unnamed, -iter)
-  #
-  # }
-
   theColumns <- NULL
-  for(i in seq_along(columns)){
+  for(i in seq_along(allCols)){
 
-    theColumn <- columns[i]
+    theColumn <- allCols[i]
+
+    if(!theColumn %in% columns){
+      fillPrevious <- TRUE
+      next
+    }
+
     theColumns <- c(theColumns, theColumn)
 
     if(i == 1){
@@ -96,7 +101,8 @@ matchOntology <- function(table = NULL, columns = NULL, dataseries = NULL,
                   certainty = 3,
                   ontology = ontoPath,
                   matchDir = paste0(intPaths, "/meta/", type, "/"),
-                  verbose = verbose)
+                  verbose = verbose,
+                  beep = beep)
 
       # new = trimws(harmonisedConc$label); target = harmonisedConc %>% select(class, has_broader); source = dataseries; certainty = 3; ontology = ontoPath;  matchDir = paste0(intPaths, "/meta/", type, "/"); match = NULL; type = "concept"; lut = NULL; verbose = FALSE
 
@@ -108,8 +114,14 @@ matchOntology <- function(table = NULL, columns = NULL, dataseries = NULL,
           distinct() %>%
           arrange(label)
 
-        newConcepts <- get_concept(table = new, ontology = ontoPath) %>%
-          filter(has_source == srcID & class %in% theColumn) %>%
+        if(exact_class){
+          newConcepts <- get_concept(table = new, ontology = ontoPath) %>%
+            filter(has_source == srcID & class %in% theColumn)
+        } else {
+          newConcepts <- get_concept(table = new, ontology = ontoPath) %>%
+            filter(has_source == srcID)
+        }
+        newConcepts <- newConcepts %>%
           rename(target := external) %>%
           select(-class, -description, !!theColumn := target)
 
@@ -127,20 +139,66 @@ matchOntology <- function(table = NULL, columns = NULL, dataseries = NULL,
           st_drop_geometry() %>%
           select(label = all_of(theColumn), has_broader = id) %>%
           distinct() %>%
-          arrange(label)
+          arrange(label) %>%
+          mutate(lvl = length(str_split(has_broader, "[.]")[[1]]))
 
-        newConcepts <- get_concept(table = new, ontology = ontoPath) %>%
-          filter(has_source == srcID) %>%
+        filterClasses <- get_class(ontology = ontology) %>%
+          filter(label %in% "al3")
+        filterClassLevel <- length(str_split(string = filterClasses$id, pattern = "[.]")[[1]])
+
+        if(all(new$lvl < filterClassLevel-1)){
+          new <- new %>%
+            select(-has_broader)
+          targetCols <- c(theColumn, "label")
+        } else {
+          targetCols <- c(theColumn, "has_broader", "label")
+        }
+
+        if(exact_class){
+          newConcepts <- get_concept(table = new %>% select(-lvl), ontology = ontoPath) %>%
+            filter(has_source == srcID & class %in% theColumn)
+        } else {
+          newConcepts <- get_concept(table = new %>% select(-lvl), ontology = ontoPath) %>%
+            filter(has_source == srcID)
+        }
+        newConcepts <- newConcepts %>%
+          filter(has_source == srcID & class == theColumn) %>%
           rename(!!theColumn := external) %>%
           mutate(has_broader = if_else(!class %in% theColumn, id, has_broader),
                  id = if_else(!class %in% theColumn, paste0(id, get_class(ontology = ontoPath)$id[1]), id)) %>%
-          select(-class, -description)
+          select(-class, -description) %>%
+          group_by(across(all_of(targetCols))) %>%
+          summarise(match = paste0(unique(match), collapse = " | "),
+                    id = paste0(id, collapse = " | "),
+                    has_broader = paste0(unique(has_broader), collapse = " | "),
+                    has_source = paste0(unique(has_source), collapse = " | ")) %>%
+          ungroup()
 
-        toOut <- toOut %>%
-          select(-any_of(c("external", "has_broader", "match", "has_source"))) %>%
-          rename(has_broader = id) %>%
-          left_join(newConcepts, by = c(theColumn, "has_broader")) %>%
+        tempOut <- toOut %>%
+          select(-any_of(c("external", "has_broader", "match", "has_source")))
+
+        if(all(new$lvl < filterClassLevel-1)){
+          tempOut <- tempOut %>%
+            select(-id)
+        } else {
+          tempOut <- tempOut %>%
+            rename(has_broader = id)
+        }
+
+        toOut <- tempOut %>%
+          left_join(newConcepts, by = targetCols[-which(targetCols %in% "label")]) %>%
           rename(external = all_of(theColumn), !!theColumn := label)
+
+        if(fillPrevious){
+          toOut <- toOut %>%
+            select(id = has_broader) %>%
+            st_drop_geometry() %>%
+            get_concept(ontology = ontoPath) %>%
+            select(id, !!allCols[i-1] := label) %>%
+            distinct() %>%
+            filter(!is.na(id)) %>%
+            left_join(toOut, ., by = c("has_broader" = "id"))
+        }
 
       }
 
@@ -159,17 +217,18 @@ matchOntology <- function(table = NULL, columns = NULL, dataseries = NULL,
                has_source = "1",
                external = !!sym(theColumn))
     }
-  }
 
+    fillPrevious <- FALSE
+  }
 
   if(!all_cols){
     out <- toOut %>%
       select(-external, -has_broader, -match, -has_source) %>%
-      select(all_of(columns), id, everything())
+      select(all_of(allCols), id, everything())
   } else {
     out <- toOut %>%
       select(-any_of(c("has_broader"))) %>%
-      select(all_of(columns), id, match, external, has_source, everything())
+      select(all_of(allCols), id, match, external, has_source, everything())
   }
 
   return(out)
