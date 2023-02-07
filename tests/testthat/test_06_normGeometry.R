4bfa57cd376afbc88968b1d4e9af6d422b5bfa3f
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2023-02-07 03:59:00 +0100

    update tests

--- tests/testthat/test_06_normGeometry.R
+++ tests/testthat/test_06_normGeometry.R
@@ -2,6 +2,7 @@ library(sf)
 library(testthat)
 library(checkmate)
 library(arealDB)
+library(ontologics)
 context("normGeometry")
 
 test_that("geometries can be normalised", {
@@ -14,9 +15,9 @@ test_that("geometries can be normalised", {
   # normalise first level ----
   output <- normGeometry(input = paste0(getOption("adb_path"), "/adb_geometries/stage2/_al1__gadm.gpkg"),
                          update = TRUE)
-  expect_tibble(x = output, nrows = 1, ncols = 12, col.names = "strict")
-  expect_names(x = names(output), must.include = c("geoID", "datID", "label", "source_file",
-                                                   "layer", "hierarchy", "orig_file", "orig_link",
+  expect_tibble(x = output, nrows = 1, ncols = 11, col.names = "strict")
+  expect_names(x = names(output), must.include = c("geoID", "datID", "source_file",
+                                                   "layer", "label", "orig_file", "orig_link",
                                                    "download_date", "next_update", "update_frequency", "notes"))
   expect_file_exists(x = paste0(getOption("adb_path"), "/adb_geometries/stage2/processed/_al1__gadm.gpkg"))
 
@@ -24,14 +25,19 @@ test_that("geometries can be normalised", {
   expect_class(x = final, classes = c("sf"))
   expect_data_frame(x = final, nrows = 1, ncols = 7)
   expect_names(x = names(final), identical.to = c("gazID", "gazName", "gazClass", "match", "external", "geoID", "geom"))
-
+  onto <- load_ontology(path = paste0(dbpath, "/territories.rds"))
+  expect_set_equal(x = onto@concepts$harmonised$label,
+                   y = c("a_nation", "county_1", "municipality1_1", "municipality1_2", "county_2", "municipality2_1",
+                         "county_3", "municipality3", "county_4", "municipality4_1", "municipality4_2"))
+  expect_set_equal(x = onto@concepts$harmonised$has_close_match,
+                   y = c("gadm_1.3",NA, NA, NA, NA, NA, NA, NA, NA, NA, NA), )
 
   # normalise second level ----
   output <- normGeometry(input = paste0(getOption("adb_path"), "/adb_geometries/stage2/_al2__gadm.gpkg"),
                          update = TRUE)
-  expect_tibble(x = output, nrows = 1, ncols = 12, col.names = "strict")
+  expect_tibble(x = output, nrows = 1, ncols = 11, col.names = "strict")
   expect_names(x = names(output), must.include = c("geoID", "datID", "label", "source_file",
-                                                   "layer", "hierarchy", "orig_file", "orig_link",
+                                                   "layer", "orig_file", "orig_link",
                                                    "download_date", "next_update", "update_frequency", "notes"))
   expect_file_exists(x = paste0(getOption("adb_path"), "/adb_geometries/stage2/processed/_al2__gadm.gpkg"))
 
@@ -39,28 +45,52 @@ test_that("geometries can be normalised", {
   expect_class(x = final, classes = c("sf"))
   expect_data_frame(x = final, nrows = 4, ncols = 7)
   expect_names(x = names(final), identical.to = c("gazID", "gazName", "gazClass", "match", "external", "geoID", "geom"))
-
+  onto <- load_ontology(path = paste0(dbpath, "/territories.rds"))
+  expect_set_equal(x = onto@concepts$harmonised$label,
+                   y = c("a_nation", "county_1", "municipality1_1", "municipality1_2", "county_2", "municipality2_1",
+                         "county_3", "municipality3", "county_4", "municipality4_1", "municipality4_2"))
+  expect_set_equal(x = onto@concepts$harmonised$has_close_match,
+                   y = c("gadm_1.3","gadm_2.3", NA, NA, "gadm_3.3", NA, "gadm_4.3", NA, "gadm_5.3", NA, NA), )
 
   # normalise third level ----
   output <- normGeometry(input = paste0(getOption("adb_path"), "/adb_geometries/stage2/_al3__gadm.gpkg"),
                          update = TRUE)
-  expect_tibble(x = output, nrows = 1, ncols = 12, col.names = "strict")
+  expect_tibble(x = output, nrows = 1, ncols = 11, col.names = "strict")
   expect_names(x = names(output), must.include = c("geoID", "datID", "label", "source_file",
-                        "layer", "hierarchy", "orig_file", "orig_link",
+                        "layer", "orig_file", "orig_link",
                         "download_date", "next_update", "update_frequency", "notes"))
   expect_file_exists(x = paste0(getOption("adb_path"), "/adb_geometries/stage2/processed/_al3__gadm.gpkg"))
 
   # normalise a non-gadm dataset that has been attached to the DB ----
   output <- normGeometry(input = paste0(getOption("adb_path"), "/adb_geometries/stage2/_al3__madeUp.gpkg"),
+                         priority = "spatial",
                          update = TRUE)
-  expect_tibble(x = output, nrows = 1, ncols = 12, col.names = "strict")
+  expect_tibble(x = output, nrows = 1, ncols = 11, col.names = "strict")
   expect_names(x = names(output), must.include = c("geoID", "datID", "label", "source_file",
-                        "layer", "hierarchy", "orig_file", "orig_link",
+                        "layer", "orig_file", "orig_link",
                         "download_date", "next_update", "update_frequency", "notes"))
 
   final <- st_read(dsn = paste0(getOption("adb_path"), "/adb_geometries/stage3/a_nation.gpkg"), layer = "al3", quiet = TRUE)
   expect_class(x = final, classes = c("sf"))
-  expect_data_frame(x = final, nrows = 13, ncols = 7)
+  expect_data_frame(x = final, nrows = 12, ncols = 7)
   expect_names(x = names(final), identical.to = c("gazID", "gazName", "gazClass", "match", "external", "geoID", "geom"))
+  expect_names(x = final$gazID, identical.to = c(".001.001.001", ".001.001.002", ".001.001.003", ".001.001.004", ".001.002.001",
+                                                 ".001.002.001", ".001.003.001", ".001.003.001", ".001.004.001", ".001.004.001",
+                                                 ".001.004.002", ".001.004.002"))
+  expect_names(x = final$match, identical.to = c("exact", "exact", "broader [83<>100_.001.001.001] | broader [17<>20_.001.001.002]",
+                                                 "narrower [100<>80_.001.001.002]", "exact", "close [100<>100_.001.002.001]",
+                                                 "exact", "close [100<>100_.001.003.001]", "exact", "close [100<>100_.001.004.001]",
+                                                 "exact", "close [100<>100_.001.004.002]"))
+  onto <- load_ontology(path = paste0(dbpath, "/territories.rds"))
+  expect_set_equal(x = onto@concepts$harmonised$label,
+                   y = c("a_nation", "county_1", "municipality1_1", "municipality1_2", "county_2", "municipality2_1",
+                         "county_3", "municipality3", "county_4", "municipality4_1", "municipality4_2"))
+  expect_set_equal(x = onto@concepts$harmonised$has_close_match,
+                   y = c("gadm_1.3 | madeUp_1.3","gadm_2.3", NA, NA, "gadm_3.3", "madeUp_2.3", "gadm_4.3",
+                         "madeUp_3.3", "gadm_5.3", "madeUp_4.3", "madeUp_5.3"))
+  expect_set_equal(x = onto@concepts$harmonised$has_broader_match,
+                   y = c(NA, NA, NA, NA, "madeUp_7.3", "madeUp_7.3", NA, NA, NA, NA, NA))
+  expect_set_equal(x = onto@concepts$harmonised$has_narrower_match,
+                   y = c(NA, NA, NA, "madeUp_6.3", NA, NA, NA, NA, NA, NA, NA))
 
 })
--- tests/testthat/test_07_normTable.R
+++ tests/testthat/test_07_normTable.R
@@ -3,16 +3,28 @@ library(testthat)
 library(tabshiftr)
 library(readr)
 library(checkmate)
+library(ontologics)
 context("normTable")
 
 
 test_that("tables can be normalised (without matched variables)", {
 
   dbpath <- paste0(tempdir(), "/newDB")
-  makeExampleDB(until = "normGeometry", path = dbpath)
+  makeExampleDB(until = "normGeometry", path = dbpath) #here it is sorting in Gemeinde 1.3 and 1.4 wrong (with has_broader .001)
 
   output <- normTable(input = paste0(getOption("adb_path"), "/adb_tables/stage2/_al1_barleyMaize_1990_2017_madeUp.csv"),
                       update = TRUE)
+  onto <- load_ontology(path = paste0(dbpath, "/territories.rds"))
+  expect_set_equal(x = onto@concepts$harmonised$label,
+                   y = c("a_nation", "county_1", "municipality1_1", "municipality1_2", "county_2", "municipality2_1",
+                         "county_3", "municipality3", "county_4", "municipality4_1", "municipality4_2"))
+  expect_set_equal(x = onto@concepts$harmonised$has_close_match,
+                   y = c("gadm_1.3 | madeUp_1.3", "gadm_2.3 | madeUp_2.3", NA, NA, "gadm_3.3 | madeUp_3.3", "madeUp_6.3",
+                         "gadm_4.3 | madeUp_4.3", "madeUp_7.3", "gadm_5.3 | madeUp_5.3", "madeUp_8.3", "madeUp_9.3"))
+  expect_set_equal(x = onto@concepts$harmonised$has_broader_match,
+                   y = c(NA, NA, NA, NA, "madeUp_11.3", "madeUp_11.3", NA, NA, NA, NA, NA))
+  expect_set_equal(x = onto@concepts$harmonised$has_narrower_match,
+                   y = c(NA, NA, NA, "madeUp_10.3", NA, NA, NA, NA, NA, NA, NA))
 
   # test whether the resulting file is "correct" ----
   expect_file_exists(x = paste0(getOption("adb_path"), "/adb_tables/stage2/processed/_al1_barleyMaize_1990_2017_madeUp.csv"))
