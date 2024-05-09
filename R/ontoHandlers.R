79354ff9c1799ef5cbd33481550d70e969cd9952
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2024-05-09 14:46:58 +0200

    without "/"

--- R/ontoHandlers.R
+++ R/ontoHandlers.R
@@ -167,7 +167,7 @@ matchOntology <- function(table = NULL, columns = NULL, dataseries = NULL,
                                       target = toMatch %>% select(has_broader, class),
                                       source = dataseries,
                                       ontology = ontology,
-                                      matchDir = paste0(ontoMatching, "/"),
+                                      matchDir = ontoMatching,
                                       verbose = verbose,
                                       beep = beep)
 
