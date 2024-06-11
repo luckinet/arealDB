0e13726e1e88cd2b0e7638e90cec5a01f8e7f2a8
EhrmannS <steffen.ehrmann@posteo.de>
2024-06-11 02:16:05 +0200

    new function (no content yet)

---
+++ R/adb_validate.R
@@ -0,0 +1,16 @@
+#' Validate files
+#'
+#'
+
+adb_validate <- function(){
+
+  # set internal paths
+  intPaths <- paste0(getOption(x = "adb_path"))
+
+  # validate the following files:
+  # - inventory of tables, geometries and dataseries
+  # - matching tables of geometries and tables
+  # - perhaps compare ontology with matching tables?!
+
+
+}
