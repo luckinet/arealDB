a17731b57906714ba9eaecac5e95accc7154c072
EhrmannS <steffen.ehrmann@posteo.de>
2025-02-11 19:12:40 +0100

    adapt tests to the additional column

--- tests/testthat/test_03_normTable.R
+++ tests/testthat/test_03_normTable.R
@@ -19,8 +19,8 @@ test_that("tables can be normalised (without matched variables)", {
   expect_file_exists(x = paste0(getOption("adb_path"), "/tables/stage2/processed/_al1_barleyMaize_1990_2017_madeUp.csv"))
   final <- readRDS(file = paste0(getOption("adb_path"), "/tables/stage3/a_nation.rds"))
   expect_tibble(x = final, types = c("integer", "integer", "integer", "character", "integer", "character", "double", "double"))
-  expect_data_frame(x = final, nrows = 56, ncols = 9)
-  expect_names(x = names(final), identical.to = c("tabID", "geoID", "gazID", "gazName", "gazMatch", "year", "commodity", "harvested", "production"))
+  expect_data_frame(x = final, nrows = 56, ncols = 10)
+  expect_names(x = names(final), identical.to = c("tabID", "geoID", "gazID", "gazName", "gazMatch", "gazClass", "year", "commodity", "harvested", "production"))
 
 })
 
@@ -35,7 +35,7 @@ test_that("tables can be normalised (with matched variables)", {
   expect_file_exists(x = paste0(getOption("adb_path"), "/tables/stage2/processed/_al1_barleyMaize_1990_2017_madeUp.csv"))
   expect_file_exists(x = paste0(getOption("adb_path"), "/tables/stage2/processed/aNation_al2_barleyMaize_1990_2017_madeUp.csv"))
   final <- readRDS(file = paste0(getOption("adb_path"), "/tables/stage3/a_nation.rds"))
-  expect_data_frame(x = final, nrows = 280, ncols = 9)
-  expect_names(x = names(final), identical.to = c("tabID", "geoID", "gazID", "gazName", "gazMatch", "year", "commodity", "harvested", "production"))
+  expect_data_frame(x = final, nrows = 280, ncols = 10)
+  expect_names(x = names(final), identical.to = c("tabID", "geoID", "gazID", "gazName", "gazMatch", "gazClass", "year", "commodity", "harvested", "production"))
 
 })
